090e7b0a6746dc896cd73954b4c3b15803a34e74
gustav <mrbingofuel0@gmail.com>
2026-01-27 11:18:19 +0100

    updated all makefiles

--- change-all-makefiles.py
+++ change-all-makefiles.py
@@ -20,40 +20,39 @@ def main():
 
     start_time = time.time()
 
-    # for proj in projects:
-    proj = projects[0]
-    proj_name = proj.name
-    print(f"Enter ==> {proj_name}")
-    makefile = proj / "Makefile"
+    for proj in projects:
+        proj_name = proj.name
+        print(f"Enter ==> {proj_name}")
+        makefile = proj / "Makefile"
 
-    if makefile.exists():
-        with open(str(makefile), "r+") as mf:
-            lines = mf.readlines()
-            cc_line = -1
-            gpu_line = -1
-            new_cc_line = ""
-            new_gpu_line = ""
-            for i, line in enumerate(lines):
-                if "CC" in line and "=" in line and "clang++" in line:
-                    cc_line = i
-                    new_cc_line = line.replace("clang++", "$(CXX)")
-                    continue
-                if "GPU" in line and "=" in line and "yes" in line:
-                    gpu_line = i
-                    new_gpu_line = line.replace("yes", "$(USE_GPU)")
-                    break;
-            if cc_line >= 0:
-                lines[cc_line] = new_cc_line
-            if gpu_line >= 0:
-                lines[gpu_line] = new_gpu_line 
-            mf.seek(0)
-            for line in lines:
-                mf.write(line)
-            mf.truncate()
-            print("Content modified!")
-    else:
-        print(f'File not found: "{makefile}"')
-    print("Exit")
+        if makefile.exists():
+            with open(str(makefile), "r+") as mf:
+                lines = mf.readlines()
+                cc_line = -1
+                gpu_line = -1
+                new_cc_line = ""
+                new_gpu_line = ""
+                for i, line in enumerate(lines):
+                    if "CC" in line and "=" in line and "clang++" in line:
+                        cc_line = i
+                        new_cc_line = line.replace("clang++", "$(CXX)")
+                        continue
+                    if "GPU" in line and "=" in line and "yes" in line:
+                        gpu_line = i
+                        new_gpu_line = line.replace("yes", "$(USE_GPU)")
+                        break;
+                if cc_line >= 0:
+                    lines[cc_line] = new_cc_line
+                if gpu_line >= 0:
+                    lines[gpu_line] = new_gpu_line 
+                mf.seek(0)
+                for line in lines:
+                    mf.write(line)
+                mf.truncate()
+                print("Content modified!")
+        else:
+            print(f'File not found: "{makefile}"')
+        print("Exit")
             
 
 
