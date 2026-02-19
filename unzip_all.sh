c93656968904ab26f34538e39d1e97b727b76ad9
gustav <mrbingofuel0@gmail.com>
2026-02-19 15:09:13 +0100

    fixed unzip script more

--- unzip_all.sh
+++ unzip_all.sh
@@ -4,21 +4,21 @@ set -euo pipefail
 TARGET_DIR="${1:-src}"
 
 find "$TARGET_DIR" -type f \( \
-  -name "*.tar.gz" -o -name "*.tar.bz2" -o \
-  -name "*.gz" -o -name "*.bz2" \
+  -name "*.tar.gz" -o -name "*.tar.bz2" -o -name "*.tar.bz" -o \
+  -name "*.gz" -o -name "*.bz2" -o -name "*.bz" \
   \) -exec sh -c '
   for file; do
     dir=$(dirname "$file")
     echo "Processing: $file"
 
     case "$file" in
-      *.tar.gz|*.tar.bz2)
+      *.tar.gz|*.tar.bz2|*.tar.bz)
         tar -xf "$file" -C "$dir"
         ;;
       *.gz)
         gunzip -k "$file"
         ;;
-      *.bz2)
+      *.bz2|*.bz)
         bunzip2 -k "$file"
         ;;
     esac
