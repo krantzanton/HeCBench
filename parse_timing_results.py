8143b450d7da75aa2b57d2605bc37e46577aeeff
gustav <mrbingofuel@gmail.com>
2026-03-06 10:18:48 +0100

    finished parsing script

--- parse_timing_results.py
+++ parse_timing_results.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 import argparse
-from os.path import exists
 import re
 import sys
-import time
 from pathlib import Path
 
+
 def main():
-    ap = argparse.ArgumentParser(description="parse all *-sycl benchmarks for ocl and omp.")
-    ap.add_argument("--result-ocl", default=".", help="Root directory containing ocl results (default: .)")
-    ap.add_argument("--result-omp", default=".", help="Root directory containing omp sycl results (default: .)")
+    ap = argparse.ArgumentParser(
+        description="parse all *-sycl benchmarks for ocl and omp."
+    )
+    ap.add_argument(
+        "--result-ocl",
+        default=".",
+        help="Root directory containing ocl results (default: .)",
+    )
+    ap.add_argument(
+        "--result-omp",
+        default=".",
+        help="Root directory containing omp sycl results (default: .)",
+    )
     args = ap.parse_args()
 
     result_ocl_root = Path(args.result_ocl).resolve()
@@ -19,63 +28,80 @@ def main():
     projects_ocl = sorted([p for p in result_ocl_root.glob("*-sycl") if p.is_dir()])
     projects_omp = sorted([p for p in result_omp_root.glob("*-sycl") if p.is_dir()])
     if not projects_ocl:
-        print(f"No projects found under {result_ocl_root} matching {args.pattern}", file=sys.stderr)
+        print(
+            f"No projects found under {result_ocl_root} matching {args.pattern}",
+            file=sys.stderr,
+        )
         sys.exit(1)
     if not projects_omp:
-        print(f"No projects found under {result_omp_root} matching {args.pattern}", file=sys.stderr)
+        print(
+            f"No projects found under {result_omp_root} matching {args.pattern}",
+            file=sys.stderr,
+        )
         sys.exit(1)
     result_file = Path.cwd() / "timing_results.csv"
-    for proj in list(zip(projects_ocl, projects_omp)):
-        proj_name = proj[0].name
-        print(f"Enter ==> {proj_name}")
-        runlog_ocl = proj[0] / "run.log"
-        runlog_omp = proj[1] / "run.log"
-        if runlog_ocl.exists() and runlog_omp.exists():
-            ocl_log = open(runlog_ocl, "r")
-            omp_log = open(runlog_omp, "r")
-            content_ocl = ocl_log.read()
-            content_omp = omp_log.read()
-            sums_ocl = re.findall(r"\[SYCL\]\[sum\][a-z\s]+(\d): ([\d\.]+)", content_ocl)
-            avgs_ocl = re.findall(r"\[SYCL\]\[avg\][a-z\s]+(\d): ([\d\.]+)", content_ocl)
-            sums_omp = re.findall(r"\[SYCL\]\[sum\][a-z\s]+(\d): ([\d\.]+)", content_omp)
-            avgs_omp = re.findall(r"\[SYCL\]\[avg\][a-z\s]+(\d): ([\d\.]+)", content_omp)
-            print(proj_name + ":")
-            print("\tsums:")
-            for result in list(zip(sums_ocl, sums_omp)):
-                ocl_kernel = result[0][0]
-                omp_kernel = result[1][0]
-                ocl_value = result[0][1]
-                omp_value = result[1][1]
-                if ocl_kernel == omp_kernel:
-                    winner = ""
-                    if float(ocl_value) > float(omp_value):
-                        winner = "ocl" 
-                    elif float(ocl_value) == float(omp_value):
-                        winner = "same"
-                    else:
-                        winner = "omp"
-                    print(f"  {'Kernel':<10} | {'ocl':<10} | {'omp':<10} | {'difference':<10} | {'fastest':<10}")
-                    print(f"  {str(ocl_kernel):<10} | {str(ocl_value):<10} | {omp_value:<10} | {str(abs(float(ocl_value)-float(omp_value))):<10} | {winner:<10}")
-            print("\tavgs:")
-            for result in list(zip(avgs_ocl, avgs_omp)):
-                ocl_kernel = result[0][0]
-                omp_kernel = result[1][0]
-                ocl_value = result[0][1]
-                omp_value = result[1][1]
-                if ocl_kernel == omp_kernel:
-                    winner = ""
-                    if float(ocl_value) > float(omp_value):
-                        winner = "ocl" 
-                    elif float(ocl_value) == float(omp_value):
-                        winner = "same"
-                    else:
-                        winner = "omp"
-                    print(f"  {'Kernel':<10} | {'ocl':<10} | {'omp':<10} | {'difference':<10} | {'fastest':<10}")
-                    print(f"  {str(ocl_kernel):<10} | {str(ocl_value):<10} | {omp_value:<10} | {str(abs(float(ocl_value)-float(omp_value))):<10} | {winner:<10}")
-        else:
-            print("run.log not found for both")
-        print("Exit")
-            
+    with open(str(result_file), "w") as output:
+        output.write(
+            "name,kernel,ocl_total,omp_total,ocl_avg,omp_avg,total_delta,avg_delta,fastest_total,fastest_avg\n"
+        )
+        for proj in list(zip(projects_ocl, projects_omp)):
+            proj_name = proj[0].name
+            print(f"Enter ==> {proj_name}")
+            runlog_ocl = proj[0] / "run.log"
+            runlog_omp = proj[1] / "run.log"
+            if runlog_ocl.exists() and runlog_omp.exists():
+                ocl_log = open(runlog_ocl, "r")
+                omp_log = open(runlog_omp, "r")
+                content_ocl = ocl_log.read()
+                content_omp = omp_log.read()
+                sums_ocl = re.findall(
+                    r"\[SYCL\]\[sum\][a-z\s]+(\d): ([\d\.]+)", content_ocl
+                )
+                avgs_ocl = re.findall(
+                    r"\[SYCL\]\[avg\][a-z\s]+(\d): ([\d\.]+)", content_ocl
+                )
+                sums_omp = re.findall(
+                    r"\[SYCL\]\[sum\][a-z\s]+(\d): ([\d\.]+)", content_omp
+                )
+                avgs_omp = re.findall(
+                    r"\[SYCL\]\[avg\][a-z\s]+(\d): ([\d\.]+)", content_omp
+                )
+                print(proj_name + ":")
+                print(" sums:")
+                print(
+                    f"  {'Kernel':<10} | {'ocl_total':<10} | {'omp_total':<10} | {'ocl_avg':<10} | {'omp_avg':<10} | {'total_delta':<16} | {'avg_delta':<16} | {'fastest_total':<16} | {'fastest_avg':<16}"
+                )
+                for result in list(zip(sums_ocl, sums_omp, avgs_ocl, avgs_omp)):
+                    ocl_kernel = result[0][0]
+                    omp_kernel = result[1][0]
+                    ocl_total = result[0][1]
+                    omp_total = result[1][1]
+                    ocl_avg = result[2][1]
+                    omp_avg = result[3][1]
+                    if ocl_kernel == omp_kernel:
+                        total_winner = ""
+                        avg_winner = ""
+                        if float(ocl_total) > float(omp_total):
+                            total_winner = "ocl"
+                        elif float(ocl_total) == float(omp_total):
+                            total_winner = "same"
+                        else:
+                            avg_winner = "omp"
+                        if float(ocl_avg) > float(omp_avg):
+                            avg_winner = "ocl"
+                        elif float(ocl_avg) == float(omp_avg):
+                            avg_winner = "same"
+                        else:
+                            avg_winner = "omp"
+                        print(
+                            f"  {str(ocl_kernel):<10} | {str(ocl_total):<10} | {omp_total:<10} | {str(ocl_avg):<10} | {omp_avg:<10} | {str(abs(float(ocl_total) - float(omp_total))):<16} | {str(abs(float(ocl_avg) - float(omp_avg))):<16} | {total_winner:<16} | {avg_winner:<16}"
+                        )
+                        output.write(
+                            f"{str(proj_name)},{str(ocl_kernel)},{str(ocl_total)},{omp_total},{str(ocl_avg)},{omp_avg},{str(abs(float(ocl_total) - float(omp_total)))},{str(abs(float(ocl_avg) - float(omp_avg)))},{total_winner},{avg_winner}\n"
+                        )
+            else:
+                print("run.log not found for both")
+            print("Exit")
 
 
 if __name__ == "__main__":
